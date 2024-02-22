b275ed639d399aeaac6b73c7775770a5d4a5b5f9
Moustapha <88747372+csof3cen@users.noreply.github.com>
2024-02-22 23:24:31 +0000

    feat: add guess the number features

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number");
-    println!("What is your number (between 1 and 100) ? ");
+    let mut tries = 0;
+    let random_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut user_number = String::new();
+    loop {
+        tries += 1;
 
-    io::stdin()
-        .read_line(&mut user_number)
-        .expect("Error reading using input.");
+        println!("What is your guess (between 1 and 100) ? ");
 
-    println!("Your entered {}", user_number);
+        let mut user_number = String::new();
+
+        io::stdin()
+            .read_line(&mut user_number)
+            .expect("Error reading using input.");
+
+        let user_number: u32 = match user_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number (in given range)!");
+                continue;
+            }
+        };
+
+        match user_number.cmp(&random_number) {
+            Ordering::Greater => println!("Too big !"),
+            Ordering::Less => println!("Too small !"),
+            Ordering::Equal => {
+                println!("You win 🎉🎊 ! Mystery number found in {} tries.", tries);
+                break;
+            }
+        }
+    }
 }
