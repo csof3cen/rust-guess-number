c5d0f622ca16d0e57c5d6ea651904b9fc3485012
Moustapha <88747372+csof3cen@users.noreply.github.com>
2024-02-22 23:05:36 +0000

    deps(add): rand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    println!("What is your number (between 1 and 100) ? ");
+
+    let mut user_number = String::new();
+
+    io::stdin()
+        .read_line(&mut user_number)
+        .expect("Error reading using input.");
+
+    println!("Your entered {}", user_number);
 }
